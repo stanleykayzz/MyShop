98a6904dcba7288409a1095869c6fd0e13e739bb
stanleykayzz <ondzoungabruce@hotmail.fr>
2024-06-05 11:44:49 +0200

    adding Command to add product

---
+++ MyShop.Command/Handler/AddProductCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MyShop.Core.Entities;
+using MyShop.Core.Interface;
+
+namespace MyShop.Command.Handler
+{
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, Unit>
+    {
+        private readonly IRepository<Product> _repository;
+
+        public AddProductCommandHandler(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.AddAsync(new Product
+            {
+                Name = request.Name,
+                Price = request.Price,
+                Quantity = request.Quantity,
+                Size = request.Size
+            });
+
+            return Unit.Value;
+        }
+    }
+}
