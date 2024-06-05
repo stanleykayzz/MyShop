edf1f5d4ceef8ae83a3a4adbaaa1d373881da92c
stanleykayzz <ondzoungabruce@hotmail.fr>
2024-06-05 10:56:14 +0200

    Creating repository interface

---
+++ MyShop.Core/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MyShop.Core.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsynct(T entity);
+    }
+}
