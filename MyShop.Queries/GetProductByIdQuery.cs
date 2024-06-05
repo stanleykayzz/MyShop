c04b15753d7804a7be9c2fb882e5f9d1bf253784
stanleykayzz <ondzoungabruce@hotmail.fr>
2024-06-05 10:57:23 +0200

    Adding mediatR to project and creating queries

---
+++ MyShop.Queries/GetProductByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MyShop.Core.Entities;
+
+namespace MyShop.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+
+        public GetProductByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
