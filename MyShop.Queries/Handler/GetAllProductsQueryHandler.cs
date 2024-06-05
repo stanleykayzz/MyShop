92c0caa3a2332c0e7b85cf59ac7bbb749f41e3aa
stanleykayzz <ondzoungabruce@hotmail.fr>
2024-06-05 10:57:50 +0200

    Adding QueryHandlers

---
+++ MyShop.Queries/Handler/GetAllProductsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MyShop.Core.Entities;
+using MyShop.Core.Interface;
+
+namespace MyShop.Queries.Handler
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IRepository<Product> _repository;
+
+        public GetAllProductsQueryHandler(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetAllAsync();
+        }
+    }
+}
---
+++ MyShop.Queries/Handler/GetProductByIdQuerydHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using MyShop.Core.Entities;
+using MyShop.Core.Interface;
+
+namespace MyShop.Queries.Handler
+{
+    public class GetProductByIdQuerydHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IRepository<Product> _repository;
+        public GetProductByIdQuerydHandler(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetByIdAsync(request.Id);
+        }
+    }
+}
