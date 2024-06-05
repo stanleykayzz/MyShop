a68a80b63cecac61cd68232e6e51cd8e2ce20bf0
stanleykayzz <ondzoungabruce@hotmail.fr>
2024-06-05 10:55:40 +0200

    product entity

---
+++ MyShop.Core/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public string? Size { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
