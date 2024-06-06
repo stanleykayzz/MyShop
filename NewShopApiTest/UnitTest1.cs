e62884bb18c3dd76d58666f8377765da89ca0ce3
stanleykayzz <ondzoungabruce@hotmail.fr>
2024-06-06 14:33:32 +0200

    Adding unit test

---
+++ NewShopApiTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NewShopApiTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
